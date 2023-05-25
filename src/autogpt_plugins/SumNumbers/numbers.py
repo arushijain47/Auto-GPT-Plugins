84b599204abed42c3955c644e9906617030b8d52
arushijain47 <116170253+arushijain47@users.noreply.github.com>
2023-05-25 17:41:58 +0530

    Update numbers.py

--- src/autogpt_plugins/SumNumbers/numbers.py
+++ src/autogpt_plugins/SumNumbers/numbers.py
@@ -10,8 +10,14 @@ def get_sum_numbers():
     #Get the data
     num1 = int(input('Enter first number: '))
     num2 = int(input('Enter second number: '))
-    Tsum = sum(num1,num2)
+
+    #Convert the integers to lists
+    num1_list = list(num1)
+    num2_list = list(num2)
     
+    #Sum the lists
+    Tsum = sum(num1_list, num2_list)
+
     #Convert it to JSON
     data = Tsum.json()
     #Extract the number and return it
